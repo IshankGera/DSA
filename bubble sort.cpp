745ecc576e6e37b681208227f243a129ce16d4c3
IshankGera <90376163+IshankGera@users.noreply.github.com>
2022-07-05 22:39:00 +0530

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,13 @@
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 1 ; i<n ; ++i)
+    {
+        for(int j = 0 ; j < n-i ; ++j)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                swap( &arr[j+1] , &arr[j]) ; 
+            }
+        }
+    }
+}
